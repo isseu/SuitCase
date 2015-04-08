5682a255c4efa4a571da2cf34986bfef47ec6f89
Enrique Correa <quiquecorreav@gmail.com>
2015-04-08 13:16:52 -0300

    rut validation y algunos cambios en interfaz

--- website/app/models/user.rb
+++ website/app/models/user.rb
@@ -28,6 +28,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  validates :rut, rut: { message: 'no es valido'}
 
   # Posibles roles de cada usuario
   ROLES = %w[guest secretary lawyer admin]
